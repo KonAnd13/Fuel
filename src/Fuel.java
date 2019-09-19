582ec1b00f915eedc364bfc3bf1be815105493a4
Andrew <55397891+KonAnd13@users.noreply.github.com>
2019-09-20 01:51:12 +0300

    Update Fuel.java

--- src/Fuel.java
+++ src/Fuel.java
@@ -3,11 +3,11 @@ Created by Konshin Andrew
 */
 public class Fuel {
     public static void main(String[] args) {
-        int ConditionalDistance = 100;
+        int conditionalDistance = 100;
         double fuelConsumption = 9.6;
         int currentFuelVolume = 35;
 
-        int resultingDistance = (int) (currentFuelVolume/fuelConsumption * ConditionalDistance);
+        int resultingDistance = (int) (currentFuelVolume/fuelConsumption * conditionalDistance);
         System.out.println(resultingDistance + " km");
     }
 }
